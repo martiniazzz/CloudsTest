352d075d8109a453bba1a397f6bd322b6f12625c
martiniazzz <32032050+martiniazzz@users.noreply.github.com>
2018-02-11 18:43:26 +0200

    Second commit

---
+++ src/Testing.java
@@ -0,0 +1,18 @@
+
+public class Testing {
+	
+	public static void main(String[] args){
+		Testing test = new Testing(4);
+	}
+	
+	private final int value;
+	
+	public Testing(int value){
+		this.value = value;
+	}
+	
+	public int multy(int amount){
+		return this.value * amount;
+	}
+
+}
